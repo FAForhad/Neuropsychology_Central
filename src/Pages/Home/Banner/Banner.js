11c93f4dae88fc0566a0dc7202387fa526167506
FAForhad <faforhad450@gmail.com>
2023-02-02 23:55:42 +0600

    banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -10,8 +10,8 @@ const Banner = () => {
         <div>
             <div >
                 <Swiper
+
                     style={{
-                        height: '850px',
                         "--swiper-navigation-color": "#fff",
                         "--swiper-pagination-color": "#fff",
                     }}
@@ -29,7 +29,7 @@ const Banner = () => {
                         className="parallax-bg "
                         style={{
                             "backgroundImage":
-                                "url(https://i.postimg.cc/fRGrKf7n/sad-man-holding-head-with-hand-1150-6345.jpg)",
+                                "url(https://i.postimg.cc/J468Bhz9/ancient-destroyed-building-with-lot-ruins-inside-181624-6070.jpg)",
                         }}
                         data-swiper-parallax="-15%"
                     ></div>
@@ -37,12 +37,12 @@ const Banner = () => {
                         <div className="text" data-swiper-parallax="-100">
                             <div className='lg:flex md:flex items-center justify-center'>
                                 <div className='w-full text-start lg:px-8 lg:pl-14'>
-                                    <p className='lg:text-3xl text-lg md:text-xl uppercase'>Reduce <span className='text-yellow-500'>Stress</span> </p>
-                                    <p className='lg:text-6xl text-3xl md:text-4xl my-5 uppercase'>It's not stress <br /> <span className='bg-yellow-600 pr-24 px-1 rounded-lg' style={{
+                                    <p className='lg:text-3xl text-lg md:text-xl uppercase text-yellow-500 font-bold'>Reduce <span className=''>Stress</span> </p>
+                                    <p className='lg:text-6xl text-xl md:text-4xl my-5 uppercase'>It's not stress <br /> <span className='bg-yellow-600 pr-24 px-1 rounded-lg' style={{
                                         clipPath: 'polygon(0% 0%, 99% 0%, 80% 100%, 79% 100%, 0% 100%)'
                                     }}>that kills us</span><br />it's our reaction to it.</p>
                                 </div>
-                                <div className="card  w-full p-5 mx-auto lg:mt-24">
+                                <div className="card hidden lg:block  w-full p-5 mx-auto lg:mt-24">
                                     <img className='pt-6 lg:h-full mt-12 md:h-72 h-60' src="https://i.postimg.cc/sXGsxM0c/pngegg.png" alt="" />
                                 </div>
 
@@ -54,12 +54,12 @@ const Banner = () => {
                             <div className='lg:flex md:flex items-center justify-center'>
                                 <div className='w-full text-start lg:px-8 lg:pl-14'>
                                     <p className='lg:text-3xl text-lg md:text-xl uppercase'>Reduce <span className='text-yellow-500'>Stress</span> </p>
-                                    <p className='lg:text-6xl text-3xl md:text-4xl my-5 uppercase'>It's not stress <br /> <span className='bg-yellow-600 pr-24 px-1 rounded-lg' style={{
+                                    <p className='lg:text-6xl text-xl md:text-4xl my-5 uppercase'>It's not stress <br /> <span className='bg-yellow-600 pr-24 px-1 rounded-lg' style={{
                                         clipPath: 'polygon(0% 0%, 99% 0%, 80% 100%, 79% 100%, 0% 100%)'
                                     }}>that kills us</span><br />it's our reaction to it.</p>
                                 </div>
-                                <div className="card  w-full p-5 mx-auto lg:mt-24">
-                                    <img className='pt-6 lg:h-full mt-12 md:h-72 h-60' src="https://i.postimg.cc/sXGsxM0c/pngegg.png" alt="" />
+                                <div className="card hidden lg:block  w-full p-5 mx-auto lg:mt-24">
+                                    <img className='pt-6 mt-12 md:h-72 h-60' style={{ height: '620px', paddingBottom: '50px' }} src="https://i.postimg.cc/qRVr1C8F/Pngtree-depression-bad-thoughts-stress-concept-8817411.png" alt="" />
                                 </div>
 
                             </div>
@@ -70,12 +70,12 @@ const Banner = () => {
                             <div className='lg:flex md:flex items-center justify-center'>
                                 <div className='w-full text-start lg:px-8 lg:pl-14'>
                                     <p className='lg:text-3xl text-lg md:text-xl uppercase'>Reduce <span className='text-yellow-500'>Stress</span> </p>
-                                    <p className='lg:text-6xl text-3xl md:text-4xl my-5 uppercase'>It's not stress <br /> <span className='bg-yellow-600 pr-24 px-1 rounded-lg' style={{
+                                    <p className='lg:text-6xl text-xl md:text-4xl my-5 uppercase'>It's not stress <br /> <span className='bg-yellow-600 pr-24 px-1 rounded-lg' style={{
                                         clipPath: 'polygon(0% 0%, 99% 0%, 80% 100%, 79% 100%, 0% 100%)'
                                     }}>that kills us</span><br />it's our reaction to it.</p>
                                 </div>
-                                <div className="card  w-full p-5 mx-auto lg:mt-24">
-                                    <img className='pt-6 lg:h-full mt-12 md:h-72 h-60' src="https://i.postimg.cc/sXGsxM0c/pngegg.png" alt="" />
+                                <div className="card hidden lg:block w-full p-5 mx-auto lg:mt-24">
+                                    <img className='pt-6 lg:h-full mt-12 md:h-72 h-60' style={{ height: '620px', paddingBottom: '50px' }} src="https://i.postimg.cc/GLyv780x/pngwing-com-2.png" alt="" />
                                 </div>
 
                             </div>
